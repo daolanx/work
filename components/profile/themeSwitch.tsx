4151d3e2e9c743e75ccfa59a5703677680efafcc
daolanx <daolanx@hotmail.com>
2026-01-04 09:54:06 +0800

    fix(hydration): add useEffect or suppressHydrationWarning to fix hydration mismatch.

--- components/profile/themeSwitch.tsx
+++ components/profile/themeSwitch.tsx
@@ -1,10 +1,22 @@
 "use client"
-
+import { useState, useEffect } from "react"
 import { Sun, Moon } from "lucide-react"
 import { useTheme } from "next-themes"
 
 export default function ModeToggle() {
   const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    // Render a placeholder or nothing to avoid the mismatch
+    // This must match exactly what the server would output
+    return <button className="cursor-pointer"><div className="h-5 w-5" /></button>
+  }
 
   return (
     <button className=" cursor-pointer " onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
