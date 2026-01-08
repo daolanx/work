8e819e40b49e7e191d38495c28183df86865764d
daolanx <daolanx@hotmail.com>
2026-01-08 14:31:01 +0800

    feat(umami): add umami for traffic view.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,38 @@
-import { ThemeProvider } from "next-themes";
-import { NextIntlClientProvider } from "next-intl";
+import { ThemeProvider } from 'next-themes';
+import { NextIntlClientProvider } from 'next-intl';
 import { getLocale } from 'next-intl/server';
-import { ClerkProvider } from '@clerk/nextjs'
-import "@/app/globals.css";
+import Script from 'next/script';
+import { ClerkProvider } from '@clerk/nextjs';
+import '@/app/globals.css';
 
 export default async function LocaleLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const locale = await getLocale();
-  const afterSignOutUrl = process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_OUT_URL || "/";
+  const afterSignOutUrl =
+    process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_OUT_URL || '/';
 
-  return (<ClerkProvider
-    afterSignOutUrl={afterSignOutUrl}
-  >
-    <html lang={locale} suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <NextIntlClientProvider>
-            {children}
-          </NextIntlClientProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+  return (
+    <ClerkProvider afterSignOutUrl={afterSignOutUrl}>
+      <html lang={locale} suppressHydrationWarning>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <NextIntlClientProvider>{children}</NextIntlClientProvider>
+          </ThemeProvider>
+          <Script
+            src="https://cloud.umami.is/script.js"
+            data-website-id="f6e47b9c-c1f0-4c75-9bcd-843e2630e8b3"
+            strategy="afterInteractive"
+          />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
